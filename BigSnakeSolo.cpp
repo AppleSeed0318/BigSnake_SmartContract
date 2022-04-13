34d2deb3113ce4da9db38e9aa11e6c1d4f1cd682
appleseed0318 <jamesberry0318.email@gmail.com>
2022-04-13 10:07:13 +0800

    fixed errors

--- BigSnakeSolo.cpp
+++ BigSnakeSolo.cpp
@@ -1,40 +1,58 @@
 #include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+#include <eosio/system.hpp>
+#include <iterator>
+
+#include <cstdlib>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace eosio;
 using namespace std;
 
-#define pb push_back
-#define stake_period 4*60*60
+#define current_time eosio::current_time_point()
 
-class[[eosio::contract("solo")]] solo : public eosio::contract {
+class[[eosio::contract("BigSnakeSolo")]] BigSnakeSolo : public eosio::contract {
 	public:
-		solo(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+		BigSnakeSolo(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds),
+		t_bsi("BSI", 0),
+		t_bsm("BSM", 0),
+		t_bsg("BSG", 0) {}
+		// static constexpr symbol BSI_SYMBOL = symbol(symbol_code("BSI"), 4);
+		// static constexpr symbol BSM_SYMBOL = symbol(symbol_code("BSM"), 4);
+		// static constexpr symbol BSG_SYMBOL = symbol(symbol_code("BSG"), 4);
+
+		// eosio::asset bsi, bsm, bsg;
+		// bsi.symbol = BSI_SYMBOL;
+		// bsi.amount = 0;
+		// bsm.symbol = BSM_SYMBOL;
+		// bsm.amount = 0;
+		// bsg.symbol = BSG_SYMBOL;
+		// bsg.amount = 0;
 		
-		asset BSI.symbol = "BSI";
-		asset BSI.amount = 0.0000;
-		asset BSM.symbol = "BSM";
-		asset BSM.amount = 0.0000;
-		asset BSG.symbol = "BSG";
-		asset BSG.amount = 0.0000;
+		// asset bsi = asset(0, t_bsi);
+		// asset bsm = asset(0, t_bsm);
+		// asset bsg = asset(0, t_bsg);
+		
 		
 		// add person into person table
 		[[eosio::action]]
-		void add_person(name user) {
+		void addperson(name user) {
 		
 			require_auth(user);
 			
 			person_index people(get_self(), get_first_receiver().value);
-			auto iterator = people.find(user);
+			auto iterator = people.find(user.value);
 			check(iterator == people.end(), "User already exist");
 			if(iterator == people.end()) {
 				people.emplace(user, [&](auto& row) {
 					row.user = user;
-					row.balance_BSI = BSI;
-					row.balance_BSM = BSM;
-					row.balance_BSG = BSG;
+					row.balance_BSI = asset(0, symbol("BSI", 4));
+					row.balance_BSM = asset(0, symbol("BSI", 4));
+					row.balance_BSG = asset(0, symbol("BSI", 4));
 					row.level = 1;
-					row.solo_killed = 0
-					row.respawn_time = 0;
+					row.solo_killed = 0;
+					row.respawn_time = current_time_point();
 					row.health_point = 50;
 				});
 			}
@@ -42,10 +60,10 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 		
 		// remove person from person table
 		[[eosio::action]]
-		void remove_person(name user) {
+		void removeperson(name user) {
 			require_auth(user);
 			person_index people(get_self(), get_first_receiver().value);
-			auto iterator = people.find(user);
+			auto iterator = people.find(user.value);
 			check(iterator != people.end(), "User does not exist");
 			people.erase(iterator);
 			
@@ -53,17 +71,17 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 		
 		// update BSI, BSM, BSG token balances
 		[[eosio::action]]
-		void update_balance(name user, uint64_t bsi, uint64_t bsm, uint64_t bsg) {
+		void updatebalance(name user, uint64_t v_bsi, uint64_t v_bsm, uint64_t v_bsg) {
 			require_auth(user);
 			
 			person_index people(get_self(), get_first_receiver().value);
-			auto iterator = people.find(user);
+			auto iterator = people.find(user.value);
 			
 			if(iterator != people.end()) {
 				people.modify(iterator, user, [&](auto& row) {
-					row.balance_BSI.amount += bsi;
-					row.balance_BSM.amount += bsm;
-					row.balance_BSG.amount += bsg;
+					row.balance_BSI.amount += v_bsi;
+					row.balance_BSM.amount += v_bsm;
+					row.balance_BSG.amount += v_bsg;
 				});
 			}
 		}
@@ -71,16 +89,16 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 		// lost 1 BSG and alive
 		// return true: success,  false: not enough BSG token
 		[[eosio::action]]
-		bool fastRespawn(name user) {
+		bool fastrespawn(name user) {
 			require_auth(user);
 			
 			person_index people(get_self(), get_first_receiver().value);
-			auto iterator = people.find(user);
+			auto iterator = people.find(user.value);
 			
 			check(iterator != people.end(), "check user");
-			if(iterator.balance_BSG.amount > 0) 
+			if(iterator->balance_BSG.amount > 0) {
 				people.modify(iterator, user, [&](auto& row) {
-					row.respawn_time = time(0);
+					row.respawn_time = current_time;
 					row.health_point += 20;
 					row.balance_BSG.amount -= 1;
 				});
@@ -92,20 +110,24 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 		// check win or loss
 		// return 4: lose, 5: wait until respawn, 6: not enough health
 		[[eosio::action]]
-		int checkSoloWin(name user) {
+		int checksolowin(name user) {
 			
 			require_auth(user);
 			
 			person_index people(get_self(), get_first_receiver().value);
-			auto iterator = people.find(user);
+			auto iterator = people.find(user.value);
 			check(iterator != people.end(), "check user");
 			
 			// win check logic
-			srand(time(0));
-			if (rand()%3 % 2 == 0) {
-				
+
+			uint32_t rand = current_time.sec_since_epoch();
+
+			if ((rand % 3) % 2 == 0) {
+				uint32_t respawn_period = 1 * 60 * 60;
+				time_point_sec cur = current_time;
+
 				people.modify(iterator, user, [&](auto& row) {
-					row.respawn_time = time(0) + 1*60*60;
+					row.respawn_time = cur + respawn_period;
 					row.solo_killed = 0;
 					row.health_point = 0;
 				});
@@ -113,26 +135,26 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 			}
 			
 			//check respawn time
-			if(iterator.respawn_time > time(0)) {
+			if(iterator->respawn_time > current_time) {
 				return 5;
 			}
 			
 			//not enough health
-			if( iterator.health_point < 5 * (level * 3 - 2) ) {
+			if( iterator->health_point < 5 * (iterator->level * 3 - 2) ) {
 				return 6;
 			}
 			
 			// increase BSM token or killed snake count
-			if(iterator.solo_killed == iterator.level*3 - 2) {
+			if(iterator->solo_killed == iterator->level*3 - 2) {
 				people.modify(iterator, user, [&](auto& row) {
 					row.level += 1;
 					row.solo_killed = 0;
-					balance_BSM.amount += iterator.level*3 - 2;
+					row.balance_BSM.amount += iterator->level*3 - 2;
 					row.health_point += 20;
 				});
 				// remove contract BSM balance in owner balance
 				person_index people(get_self(), get_first_receiver().value);
-				auto iterator_o = people.find(get_self());
+				auto iterator_o = people.find(get_self().value);
 				check(iterator_o != people.end(), "Owner does not exist");
 				
 				people.modify(iterator_o, user, [&](auto& row) {
@@ -152,41 +174,45 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 		[[eosio::action]]
 		void stake(name user, vector<uint64_t> nfts, string msg) {
 			require_auth(user);
-			stakeList_index = stakes(get_self(), get_first_receiver().value);
+			stakelist_index stakes(get_self(), get_first_receiver().value);
 			check(nfts.size()>0, "no stake data");
 			
-			auto iterator = stakes.find(user);
+			auto iterator = stakes.find(user.value);
 			
+			time_point_sec cur = current_time;
 			if(iterator != stakes.end()) { // not exist yet
 				stakes.emplace(user, [&](auto& row) {
 					row.user = user;
 					row.nfts = nfts;
-					row.t_start = time(0);
-					row.t_end = time(0) + stake_period;
-					row.t_update = time(0);
+					row.t_start = cur;
+					row.t_end = cur + stake_period;
+					row.t_update = cur;
 				});
 			}
 			else { // find exist one
 				stakes.modify(iterator, user, [&](auto& row){
-					row.t_update = time(0);
-					auto (nft : nfts) row.nfts.pb(nft);
+					row.t_update = cur;
+					for (auto nft : nfts) row.nfts.push_back(nft);
 				});
 			}
 			
+
 			//check rewarded user
 			for (auto it : stakes) {
-				if(it.t_end >= time(0)) {
+				time_point_sec cur = current_time;
+
+				if(it.t_end >= cur) {
 					
 					// set default values  --- stake start
 					stakes.modify(iterator, user, [&](auto& row){
-						row.t_update = time(0);
-						row.t_start = time(0);
-						row.t_end = time(0) + stake_period;
+						row.t_update = cur;
+						row.t_start = cur;
+						row.t_end = cur + stake_period;
 					});
 					
 					// increase BSG token
 					person_index people(get_self(), get_first_receiver().value);
-					auto iterator_p = people.find(user);
+					auto iterator_p = people.find(user.value);
 					check(iterator_p != people.end(), "User does not exist");
 					
 					people.modify(iterator_p, user, [&](auto& row) {
@@ -194,8 +220,7 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 					});
 					
 					// remove contract BSG balance in owner balance
-					person_index people(get_self(), get_first_receiver().value);
-					auto iterator_o = people.find(get_self());
+					auto iterator_o = people.find(get_self().value);
 					check(iterator_o != people.end(), "Owner does not exist");
 					
 					people.modify(iterator_o, user, [&](auto& row) {
@@ -206,7 +231,11 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 		}
 		
 	private:
-		struct [[eosio::table]] person {
+
+		const symbol t_bsi, t_bsm, t_bsg;
+		const uint32_t stake_period = 4 * 60 * 60;
+
+		TABLE person {
 			name user;
 			
 			asset balance_BSI, balance_BSM, balance_BSG;
@@ -214,15 +243,16 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 			//for solo
 			int solo_killed;
 			int level;
-			int respawn_time;
+			time_point_sec respawn_time;
+			int health_point;
 			
 			person(){
-				balance_BSI = BSI;
-				balance_BSM = BSM;
-				balance_BSG = BSG;
+				balance_BSI = asset(0, symbol("BSI", 4));
+				balance_BSM = asset(0, symbol("BSM", 4));
+				balance_BSG = asset(0, symbol("BSG", 4));
 				solo_killed = 0;
 				health_point = 50;
-				respawn_time = 0;
+				respawn_time = current_time_point();
 				level = 1;
 			}
 			
@@ -230,15 +260,16 @@ class[[eosio::contract("solo")]] solo : public eosio::contract {
 		};
 		using person_index = eosio::multi_index<"people"_n, person>;
 		
-		struct [[eosio:table]] stakeList {
+		TABLE stakelist {
 			name user;
-			vector<asset> nfts;
-			time t_start;
-			time t_end;
-			time t_update;
+			vector<uint64_t> nfts;
+			time_point_sec t_start;
+			time_point_sec t_end;
+			time_point_sec t_update;
 			
 			uint64_t primary_key() const { return user.value; }
-		}
+		};
 		
-		using stakeList_index = eosio::multi_index<"stakeLists"_n, stakeList>;
-}
+		using stakelist_index = eosio::multi_index<"stakelists"_n, stakelist>;
+};
+
